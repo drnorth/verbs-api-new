a084341b8002a15f9afa38477d98c9bdab500bd3
Bogdan <bugdonepavlov@gmail.com>
2021-02-14 23:34:06 +0300

    updated generate questions value

--- src/lessons/utils/generateQuestions.ts
+++ src/lessons/utils/generateQuestions.ts
@@ -8,24 +8,39 @@ import shuffle from "utils/shuffle";
 import { IVerb } from "types.common/verbs.types";
 
 const COUNT = 3;
+const vowelsLetter = ["a", "e", "i", "o", "u", "y"];
+const variatTrick = ["ing", "ed", "en", "own", "n", "et", "old", "ought"];
 
 export const generateQuestions = (
   lesson: ILesson,
   verbs: IVerb[],
   questions: IQuestion[]
 ) => {
-  const { action, difficult } = lesson;
+  const { action } = lesson;
 
   const filterVerbs = (curr: IQuestion) => {
     const { answerType } = curr;
+    const findedVerb = verbs.find((verb) => verb.inf === curr.verb) as IVerb;
+    const lastElement = curr.verb[curr.verb.length];
+    const getValue = () => {
+      if (vowelsLetter.includes(lastElement)) {
+        return (
+          findedVerb[answerType].slice(0, -1) +
+          getRandomInt(0, variatTrick.length)
+        );
+      }
 
-    const filtredVerbs: IVerb[] = verbs.filter(
-      (verb) => verb.inf !== curr.verb && verb.difficult === difficult
-    );
+      return findedVerb[answerType] + getRandomInt(0, variatTrick.length);
+    };
 
-    return filtredVerbs.map((verb) => ({
-      id: verb.id,
-      value: verb[answerType],
+    const list = Array.from({ length: COUNT });
+    const getRandomInt = (min: number, max: number) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    return list.map((_) => ({
+      id: Math.random().toString(16),
+      value: getValue(),
       questionId: curr.id,
     }));
   };
